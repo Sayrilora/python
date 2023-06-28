e670e9ce7b164acbfe7a69a353142499273879cf
Sayrilora <sayrilora@gmail.com>
2023-06-28 10:28:00 -0300

    acualizacion

---
+++ holamundo.py
@@ -0,0 +1,7 @@
+print("-----------------------------------------------------")
+print("ejercicio de prueba de codigo. saludo hola mundo")
+print("-----------------------------------------------------")
+
+
+print("hola mundo!!!")
+print("punto digital junio 2023!!!")
